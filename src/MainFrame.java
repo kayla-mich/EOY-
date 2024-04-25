b35b24b298d635b5025560c0c45d58c6eea3788d
kayla-mich <128745137+kayla-mich@users.noreply.github.com>
2024-04-25 15:25:06 +0000

    new

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,3 +1,32 @@
-public class MainFrame {
+import java.awt.Dimension;
+
+
+import javax.swing.*;
+
+
+public class MainFrame extends JFrame {
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First JFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //JPanel mainPanel = new JPanel();
+        //mainPanel.setBackground(new Color(128, 128, 255));
+
+
+        frame.setTitle("App Learner");
+        //Dimension minimumSize - MainFrame.main(String[])
+        //frame.setMinimumSize(Dimension minimumSize);
+        frame.setSize(300, 300);
+
+
+        frame.setMinimumSize(new Dimension(300,300));
+        frame.setVisible(true);
+
+
+    }
+
+
     
 }
+
