63c3157c1f5869f24c3bf47f1d0b94d474c58a4c
kayla-mich <s1753819@online.houstonisd.org>
2024-05-07 11:53:50 -0500

    signup screen

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,7 +1,10 @@
 import java.awt.Dimension;
 import javax.swing.*;
-
-
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//This work was done at home in my laptop
 public class MainFrame extends JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("My First JFrame");
@@ -17,6 +20,57 @@ public class MainFrame extends JFrame {
 
 
         frame.setMinimumSize(new Dimension(300,300));      
+     
+
+        //Allows  do add different componets for screen
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+
+        //Text labels and text fields
+        JLabel name= new JLabel("Name");
+        JTextField textName= new JTextField(15);
+        //makes the size of the my choice
+        textName.setPreferredSize(new Dimension(10, 25));
+        panel.add(name);
+        panel.add(textName);
+        JLabel user= new JLabel("Username");
+        JTextField textUser= new JTextField(15);
+        textUser.setPreferredSize(new Dimension(10, 25));
+        panel.add(user);
+        panel.add(textUser);
+        JLabel pass= new JLabel("Password");
+        JTextField textpass= new JTextField(15);
+        textpass.setPreferredSize(new Dimension(10, 25));
+        panel.add(pass);
+        panel.add(textpass);
+       
+        //Adds the button on the screen
+        JButton submitButton= new JButton("Submit");
+        submitButton.setPreferredSize(new Dimension(75, 30));
+        panel.add(submitButton);
+
+
+        frame.add(panel);
+
+
+        submitButton.addActionListener(new ActionListener() {
+           @Override
+            public void actionPerformed(ActionEvent e){
+                frame.dispose();
+            JFrame nextFrame = new JFrame("Next Screen");
+            nextFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            nextFrame.setSize(300, 300);
+           
+            JPanel nextPanel = new JPanel();
+            JLabel step= new JLabel("One step closer to creating your account!");
+            nextPanel.add(step);
+           
+            nextFrame.add(nextPanel);
+            nextFrame.setVisible(true);
+            }
+        });
+
         frame.setVisible(true);
 
 
