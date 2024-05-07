bf90f5ed3d6393f63175f7ca2911bf28fb3ac700
kayla-mich <s1753819@online.houstonisd.org>
2024-05-07 11:34:40 -0500

    screen

--- .vscode/Main.java
+++ .vscode/Main.java
@@ -2,7 +2,8 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("My First JFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
